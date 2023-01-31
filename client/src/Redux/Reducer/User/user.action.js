ba3c9b4a8ec88fbc49cd006ee91dbc6784a3578c
ShivaKanchi <shivakanchi111@gmail.com>
2023-01-31 17:48:57 +0530

    Redux test

--- client/src/Redux/Reducer/User/user.action.js
+++ client/src/Redux/Reducer/User/user.action.js
@@ -1,33 +1,34 @@
 import axios from 'axios'
 
-import {ADD_USER,GET_USERS} from "./user.type"
+import { ADD_USER, GET_USERS } from "./user.type"
 
-export const addUser=(userData)=> async (dispatch)=>{
-    try{
-    const user =await axios({
-    method:"POST",
-    url:`${process.env.REACT_APP_CLIENT_URL}/user/add`,
-    data:{user:userData}
-       });
-localStorage.setItem("Trialproject",JSON.stringify({user:user}))
-return dispatch({type:ADD_USER,payload:userData})
+export const addUser = (userData) => async (dispatch) => {
+    try {
+        console.log(process.env.REACT_APP_CLIENT_URL, "from axios user.action")
+        const user = await axios({
+            method: "POST",
+            url: `${process.env.REACT_APP_CLIENT_URL}/user/add`,
+            data: { user: userData }
+        });
+        localStorage.setItem("Trialproject", JSON.stringify({ user: user }))
+        return dispatch({ type: ADD_USER, payload: userData })
     }
-    catch(error){
-return dispatch({type:"ERROR",payload:error})
+    catch (error) {
+        return dispatch({ type: "ERROR", payload: error })
     }
 }
 
-export const getUsers=(userData)=> async (dispatch)=>{
-    try{
-    const user =await axios({
-    method:"GET",
-    url:`${process.env.REACT_APP_CLIENT_URL}/user/`,
-       });
-       localStorage.setItem("Trialproject",JSON.stringify({user:user}))
+export const getUsers = (userData) => async (dispatch) => {
+    try {
+        const user = await axios({
+            method: "GET",
+            url: `${process.env.REACT_APP_CLIENT_URL}/user/`,
+        });
+        localStorage.setItem("Trialproject", JSON.stringify({ user: user }))
 
-return dispatch({type:GET_USERS,payload:userData})
+        return dispatch({ type: GET_USERS, payload: userData })
     }
-    catch(error){
-return dispatch({type:"ERROR",payload:error})
+    catch (error) {
+        return dispatch({ type: "ERROR", payload: error })
     }
 }
