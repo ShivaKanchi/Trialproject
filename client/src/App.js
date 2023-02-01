fb0218e1fca8ed35f8e51619034b855248ed6aef
ShivaKanchi <shivakanchi111@gmail.com>
2023-02-01 23:14:37 +0530

    Response page setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom'
+//pages
 import Home from './Pages/Home.page';
+import Responsepage from './Pages/Response.page';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Navigate to='Home' />} />
+        <Route path='/Home' element={<Home />} />
+        <Route path='/response' element={<Responsepage />} />
+      </Routes>
+      <div className="App">
+        <Home />
+      </div>
+    </>
+
   );
 }
 
--- client/src/Pages/Home.page.jsx
+++ client/src/Pages/Home.page.jsx
@@ -3,8 +3,6 @@ import Forms from '../Components/Form/Form.component'
 import Response from '../Components/Response/Response.component'
 
 const Home = () => {
-
-
     return (
         <>
             <div className='p-6 flex justify-center'>
