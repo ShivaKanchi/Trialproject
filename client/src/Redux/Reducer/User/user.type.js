816c00fe5dd38b43f2ef018a2a538f284088bba5
ShivaKanchi <shivakanchi111@gmail.com>
2023-02-07 23:27:33 +0530

    User redux action added

--- client/src/Redux/Reducer/User/user.type.js
+++ client/src/Redux/Reducer/User/user.type.js
@@ -1,2 +1,5 @@
-export const ADD_USER="ADD_USER"
-export const GET_USERS="GET_USERS"
+export const ADD_USER = "ADD_USER"
+export const GET_USERS = "GET_USERS"
+export const SEARCH_USER = "SEARCH_USER"
+export const DELETE_USER = "DELETE_USER"
+export const UPDATE_USER = "UPDATE_USER"
