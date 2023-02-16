90d0798dc1a3dfd948ee4f7842e14d5e31e6ebcd
ShivaKanchi <shivakanchi111@gmail.com>
2023-02-16 20:14:53 +0530

    Medicine componenet

--- server/Form/Api/User/index.js
+++ server/Form/Api/User/index.js
@@ -17,17 +17,5 @@ Router.post("/add", async (req, res) => {
     }
 })
 
-// To get Users from database  
-Router.get("/all", async (req, res) => {
-    try {
-        const users = await UserModel.find();
-        if (users.length === 0) return res.status(404).json({ failed: "No Users Found" })
-        return res.status(200).json({ users })
-    }
-    catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
-})
-
 
 export default Router
