96865ebdac4016253aebb29c21f5d59d345f19bf
ShivaKanchi <shivakanchi111@gmail.com>
2023-05-19 23:03:10 +0530

    Border changes

--- client/src/Pages/Home.page.jsx
+++ client/src/Pages/Home.page.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 const Home = () => {
     const navigate = useNavigate()
     const styles = {
-        background: "linear-gradient(#fff 0 0) padding-box,   linear-gradient(180deg, #d40e0e   0%, #fff 90%) border-box",
+        background: "linear-gradient(#fff 0 0) padding-box,   linear-gradient(180deg, black   0%, #fff 90%) border-box",
         border: "1px solid transparent",
         borderRaidus: "30px",
     };
@@ -24,9 +24,9 @@ const Home = () => {
     return (
         <>
             <div className='flex flex-col items-center'>
-                <div style={styles} className='w-[30%] p-6 m-5 flex flex-wrap gap-2 items-center justify-center border bg-gray-200 rounded-md'>
+                <div style={styles} className='w-[30%] p-6 m-5 flex flex-wrap gap-2 items-center justify-center bg-gray-200 rounded-md'>
                     {
-                        pagebuttons.map(data => <button className='px-3 h-auto w-auto bg-gradient-to-b from-[#fff0] to-[#d40e0e] text-black font-mono rounded-md border-[0.3px] hover:scale-105' onClick={() => { navigate(data.link) }}>{data.name}</button>
+                        pagebuttons.map(data => <button className='px-3 h-auto w-auto bg-gradient-to-b from-[#fff0] to-black text-red-900 font-mono rounded-md border-[0.3px] hover:scale-105 hover:bg-gradient-to-t from-[#fff0] to-black' onClick={() => { navigate(data.link) }}>{data.name}</button>
                         )
                     }
                 </div>
