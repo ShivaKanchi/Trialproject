66703e60b8bb6912526d2b07d1dd6f5af9874fef
ShivaKanchi <shivakanchi111@gmail.com>
2023-05-22 23:47:43 +0530

    Some issue with mapping

--- client/src/Components/FileManager/FileManger.jsx
+++ client/src/Components/FileManager/FileManger.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
 
 
+// const [newfilefolder, setNewfilefolder] = useState(false)
 
 // type 0 = folder , type 1 = file
-filesandfolder = [
+const filesandfolders = [
     {
         type: 0,
         name: 'Study',
@@ -19,17 +21,73 @@ filesandfolder = [
                 type: 1,
                 name: 'resume',
                 count: '0',
-                contains: [
-                ],
+                contains: [],
             }
 
         ],
     }
 ]
 
+const FileFolder = (data) => {
+    console.log(data)
+    return (
+        <div className='flex flex-col'>
+
+            <div className='border p-2 flex felx-row items-center justify-between w-60'>
+                {
+                    data.type === 0 ? (
+                        <h1>📁</h1>
+                    ) : (
+                        <h1>📄</h1>
+                    )
+                }
+                <h1>{data.name}</h1>
+                <div className='flex flex-row cursor-pointer'>
+                    +
+                    <div className='hidden'>
+                        <div>
+                            +📁
+                        </div>
+                        <div>
+                            +📄
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* {
+                data?.contains?.length === 0 ? (<></>) : (
+                    <>
+                        {data.contains.map((data) => (
+                            <FileFolder data={data} />
+                        ))}
+
+                    </>
+                )
+            } */}
+        </div>
+    )
+
+
+}
+
+
 const FileManger = () => {
     return (
-        <h1>FileManger</h1>
+
+        <div className='m-10'>
+
+            <h1 className='my-5'>FileManger</h1>
+            {
+
+                filesandfolders.map((data) => (
+                    <FileFolder data={data} key={data.name} />
+                )
+                )
+
+            }
+
+        </div>
+
     )
 }
 
