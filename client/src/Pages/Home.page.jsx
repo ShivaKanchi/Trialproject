68968d7deba6fc41daac838d674896c5ea05a27c
ShivaKanchi <shivakanchi111@gmail.com>
2023-09-02 23:27:14 +0530

    home page update

--- client/src/Pages/Home.page.jsx
+++ client/src/Pages/Home.page.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import './Homepage.css'
+import React from "react";
+import "./Homepage.css";
+import hello from "../assests/hello.gif";
+
 const Home = () => {
+  return (
+    <>
+      <div className="w-full h-full flex flex-col justify-center items-center relative p-5">
+        <h1>This is home</h1>
+        <div>
+          <img src={hello} />
+        </div>
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-
-            <h1
-            >This is home</h1>
-
-
-        </>
-    )
-}
-
-export default Home
+export default Home;
