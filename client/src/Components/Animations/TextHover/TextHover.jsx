48a07110d3964602785517086bfdd3ef84616dff
ShivaKanchi <shivakanchi111@gmail.com>
2023-09-09 20:48:20 +0530

    Text hover fixed

--- client/src/Components/Animations/TextHover/TextHover.jsx
+++ client/src/Components/Animations/TextHover/TextHover.jsx
@@ -1,35 +1,40 @@
-import React from 'react'
-import './TextHover.css'
+import React, { useEffect } from "react";
+import "./TextHover.css";
 const TextHover = () => {
-    const word = "hello  everyone  how  are  you  fine  sank  you".split("");
+  useEffect(() => {
+    let letterElements = document.querySelectorAll(".letter ");
+    letterElements.forEach((letterElement) => {
+      letterElement.addEventListener("mouseenter", () => {
+        console.log("Hover");
+        letterElement.classList.add("bounce");
+      });
 
-    const letterElements = document.querySelectorAll('.animatedletter>span');
-
-    letterElements.forEach(letterElement => {
-        letterElement.addEventListener('mouseenter', () => {
-            letterElement.classList.add('bounce');
-        });
-
-        letterElement.addEventListener('mouseleave', () => {
-            setTimeout(() => {
-                letterElement.classList.remove('bounce');
-            }, 1000);
-        });
+      letterElement.addEventListener("mouseleave", () => {
+        setTimeout(() => {
+          letterElement.classList.remove("bounce");
+        }, 500);
+      });
     });
+  }, []);
 
+  const wordwithnospace = "hello everyone how are you fine sank you".replaceAll(
+    " ",
+    "0"
+  );
+  const word = wordwithnospace.split("");
+  console.log(word);
+  return (
+    <div className="m-3 flex flex-col items-center mb-5">
+      <div className="font-extrabold text-center">TextHover</div>
+      <div className="animatedletter">
+        {word.map((letter, idx) => (
+          <span key={idx} color="red" className="letter">
+            {letter == 0 ? <span> &nbsp; </span> : letter}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className='m-3 flex flex-col items-center mb-5'>
-            <div className='font-extrabold text-center'>TextHover</div>
-            <div className='animatedletter'>
-                {
-                    word.map((letter, idx) => (<span key={idx} color='red' className='letter'>{letter}</span>))
-                }
-
-            </div>
-
-        </div>
-    )
-}
-
-export default TextHover
+export default TextHover;
