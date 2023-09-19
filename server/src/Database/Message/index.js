0ba073e562f4cd526721f8a4bdde9e8a807f2d4a
ShivaKanchi <shivakanchi111@gmail.com>
2023-09-19 22:10:56 +0530

    New api integration

--- server/src/Database/Message/index.js
+++ server/src/Database/Message/index.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
-const MessageSchema = new mongoose.Schema({
-  name: { type: String },
-  email: { type: String },
-  password: { Type: String },
-  phone: { Type: Number },
-});
+const MessageSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+    email: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 export const MessageModel = mongoose.model("messages", MessageSchema);
