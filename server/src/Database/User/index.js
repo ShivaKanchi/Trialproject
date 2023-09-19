a1378a8998151e667d7460b88c24f005729b793c
ShivaKanchi <shivakanchi111@gmail.com>
2023-09-19 22:17:37 +0530

    New api integration

--- server/src/Database/User/index.js
+++ server/src/Database/User/index.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String },
-  email: { type: String },
-  password: { Type: String },
-  phone: { Type: Number },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+    email: { type: String },
+    password: { Type: String },
+    phone: { Type: Number },
+  },
+  {
+    timestamps: true,
+  }
+);
 export const UserModel = mongoose.model("users", UserSchema);
